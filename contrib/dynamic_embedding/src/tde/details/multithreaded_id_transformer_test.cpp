d22a5ace25c1ec721f24a9e47bed2ad8aff2408a
Yang Yu <josephyu@tencent.com>
2022-07-22 12:21:18 +0800

    Polish code

--- contrib/dynamic_embedding/src/tde/details/multithreaded_id_transformer_test.cpp
+++ contrib/dynamic_embedding/src/tde/details/multithreaded_id_transformer_test.cpp
@@ -1,26 +1,8 @@
-#include "catch2/catch_all.hpp"
+#include "gtest/gtest.h"
 #include "tde/details/multithreaded_id_transformer.h"
 
 namespace tde::details {
 
-TEST_CASE("tde_details_MultiThreadedIDTransformer") {
-  // Some common code here
-
-  SECTION("aspect 1") {}
-
-  SECTION("aspect 2") {}
-
-  BENCHMARK_ADVANCED("benchmark 1")(Catch::Benchmark::Chronometer meter) {
-    // some set up code here
-
-    meter.measure([]() -> int {
-      // benchmark part here. This lambda will be run many times.
-
-      // return some value related to benchmark, so the compiler will not
-      // optimize whole lambda.
-      return 0;
-    });
-  };
-}
+TEST(tde, MultiThreadedIDTransformer) {}
 
 } // namespace tde::details
