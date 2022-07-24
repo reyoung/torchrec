5125973c86c9c926f41cf2f4056b08c48f13f4bf
Yang Yu <josephyu@tencent.com>
2022-07-24 11:52:30 +0800

    Add comments

--- contrib/dynamic_embedding/src/tde/details/mixed_lfu_lru_strategy.h
+++ contrib/dynamic_embedding/src/tde/details/mixed_lfu_lru_strategy.h
@@ -5,6 +5,26 @@
 #include "tde/details/random_bits_generator.h"
 namespace tde::details {
 
+/**
+ * Mixed LFU/LRU Eviction Strategy.
+ *
+ * It evict infrequence elements first, then evict least recent usage elements.
+ *
+ * The frequency is record as the power number of 2. i.e., The freq_power is 1,
+ * then the global id is used approximately 2 times. And the freq_power is 10,
+ * then the global id is used approximately 1024 times.
+ *
+ * Internally, if the current freq_power is n, then it use random bits to
+ * generate n bits. If all these n bits are zero, then the freq_power++.
+ *
+ * All LFU/LRU information uses uint32_t to record. The uint32_t should be
+ * record in IDTransformer's map/dictionary.
+ *
+ * Use `UpdateTime` to update logical timer for LRU. It only uses lower 27 bits
+ * of time.
+ *
+ * Use `Transform` to update extended value when every time global id that used.
+ */
 class MixedLFULRUStrategy {
  public:
   using ExtendedValueType = uint32_t;
