339c5ea8ae6d867a67ee538703acac4532d5c9a8
Zilin Zhu <zilinzhu@tencent.com>
2022-07-26 10:10:43 +0000

    remove torch::random_ out of benchmark

--- contrib/dynamic_embedding/src/tde/details/multithreaded_id_transformer_benchmark.cpp
+++ contrib/dynamic_embedding/src/tde/details/multithreaded_id_transformer_benchmark.cpp
@@ -10,7 +10,9 @@ static void BM_MultiThreadedIDTransformer_Cold(benchmark::State& state) {
   torch::Tensor global_ids = torch::empty({1024, 1024}, torch::kLong);
   torch::Tensor cache_ids = torch::empty_like(global_ids);
   for (auto _ : state) {
+    state.PauseTiming();
     global_ids.random_(1e10, 2e10);
+    state.ResumeTiming();
     transformer.Transform(
         tcb::span{global_ids.template data_ptr<int64_t>(), global_ids.numel()},
         tcb::span{cache_ids.template data_ptr<int64_t>(), cache_ids.numel()});
@@ -30,7 +32,9 @@ static void BM_MultiThreadedIDTransformer_Hot(benchmark::State& state) {
   torch::Tensor global_ids = torch::empty({1024, 1024}, torch::kLong);
   torch::Tensor cache_ids = torch::empty_like(global_ids);
   for (auto _ : state) {
+    state.PauseTiming();
     global_ids.random_(1e6, 2e6);
+    state.ResumeTiming();
     transformer.Transform(
         tcb::span{global_ids.template data_ptr<int64_t>(), global_ids.numel()},
         tcb::span{cache_ids.template data_ptr<int64_t>(), cache_ids.numel()});
