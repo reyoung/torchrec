093c246b404df58dcd1cb9427de62de7a424f642
Yang Yu <josephyu@tencent.com>
2022-07-27 18:20:18 +0800

    Add factory

--- contrib/dynamic_embedding/src/tde/details/id_transformer_registry_test.cpp
+++ contrib/dynamic_embedding/src/tde/details/id_transformer_registry_test.cpp
@@ -3,7 +3,7 @@
 #include "tde/details/id_transformer_registry.h"
 namespace tde::details {
 TEST(TDE, IDTransformerRegistry) {
-  ASSERT_NE(sizeof(TransformersVariant), 0);
+  ASSERT_NE(sizeof(to_variant_t<Transformers>), 0);
 }
 
 } // namespace tde::details
