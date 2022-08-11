64c8580528c57f952fb47cc7a04769db099b71de
Yang Yu <josephyu@tencent.com>
2022-08-11 11:41:35 +0800

    refine version

--- contrib/dynamic_embedding/tools/repair_wheel.sh
+++ contrib/dynamic_embedding/tools/repair_wheel.sh
@@ -5,5 +5,5 @@ DEST_DIR=$2
 
 CUDA_SUFFIX=cu$(echo "$CUDA_VERSION" | tr '.' '_')
 WHEEL_FILENAME=$(basename "${WHEEL_FILE}")
-DEST_FILENAME=$(echo "${WHEEL_FILENAME}" | sed "s#torchrec_dynamic_embedding#torchrec_dynamic_embedding_${CUDA_SUFFIX}#g")
+DEST_FILENAME=$(echo "${WHEEL_FILENAME}" | sed -r 's#(torchrec_dynamic_embedding-[0-9]+\.[0-9]+\.[0-9]+)#\1'"${CUDA_SUFFIX}#g")
 mv  "${WHEEL_FILE}" "${DEST_DIR}/${DEST_FILENAME}"
