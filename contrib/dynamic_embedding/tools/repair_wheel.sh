75d504961bd05ed8783498f00b7a3928bfab5683
Yang Yu <josephyu@tencent.com>
2022-08-11 11:16:16 +0800

    Polish repair wheel

--- contrib/dynamic_embedding/tools/repair_wheel.sh
+++ contrib/dynamic_embedding/tools/repair_wheel.sh
@@ -7,5 +7,5 @@ echo 'torchrec_dynamic_embedding-0.0.1-cp37-cp37m-linux_x86_64.whl'
 
 CUDA_SUFFIX=cu$(echo "$CUDA_VERSION" | tr '.' '_')
 WHEEL_FILENAME=$(basename "${WHEEL_FILE}")
-DEST_FILENAME=$(echo "${WHEEL_FILENAME}" | sed "s#torchrec_dynamic_embedding#torchrec_dynamic_embedding${CUDA_SUFFIX}#g")
-mv "${DEST_DIR}/${DEST_FILENAME}" "${WHEEL_FILE}"
+DEST_FILENAME=$(echo "${WHEEL_FILENAME}" | sed "s#torchrec_dynamic_embedding#torchrec_dynamic_embedding_${CUDA_SUFFIX}#g")
+mv  "${WHEEL_FILE}" "${DEST_DIR}/${DEST_FILENAME}"
