aa7b48e9e12a1d3c8e413f942772c3e5b825c1fd
Yang Yu <reyoung@126.com>
2022-08-24 12:22:40 +0800

    Add log

--- contrib/dynamic_embedding/src/tde/id_transformer.cpp
+++ contrib/dynamic_embedding/src/tde/id_transformer.cpp
@@ -3,6 +3,8 @@
 
 namespace tde {
 
+#define TDE_DEBUG() fprintf(stderr, __FILE__ ":%d\n", __LINE__)
+
 IDTransformer::IDTransformer(int64_t num_embedding, nlohmann::json json)
     : json_(std::move(json)),
       transformer_(
@@ -16,23 +18,30 @@ c10::intrusive_ptr<TransformResult> IDTransformer::Transform(
     c10::intrusive_ptr<TensorList> cache_id_list,
     int64_t time) {
   torch::NoGradGuard no_grad;
+  TDE_DEBUG();
   TORCH_CHECK(time >= 0);
   TORCH_CHECK(global_id_list->size() == cache_id_list->size());
+  TDE_DEBUG();
   transformer_.strategy_.UpdateTime(static_cast<uint32_t>(time));
+  TDE_DEBUG();
   int64_t total_numel = 0;
+  TDE_DEBUG();
   for (int64_t i = 0; i < global_id_list->size(); ++i) {
     total_numel += (*global_id_list)[i].numel();
   }
+  TDE_DEBUG();
   try {
     ids_to_fetch_.resize(2 * total_numel);
   } catch (std::bad_alloc& ex) {
     TORCH_CHECK(
         false, "bad allocate ", ex.what(), " the total_numel=", total_numel);
   }
+  TDE_DEBUG();
   int64_t num_transformed = 0;
   for (int64_t i = 0; i < global_id_list->size(); ++i) {
     torch::Tensor global_ids = (*global_id_list)[i];
     torch::Tensor cache_ids = (*cache_id_list)[i];
+    TDE_DEBUG();
     num_transformed += transformer_.Transform(
         tcb::span{
             global_ids.template data_ptr<int64_t>(),
@@ -46,16 +55,20 @@ c10::intrusive_ptr<TransformResult> IDTransformer::Transform(
           ids_to_fetch_[2 * idx + 1] = cache_id;
         });
   }
+  TDE_DEBUG();
   bool success = num_transformed == total_numel;
   int64_t num_ids_to_fetch = num_ids_to_fetch_.load();
   if (num_ids_to_fetch == 0) {
     return c10::make_intrusive<TransformResult>(success, torch::Tensor{});
   }
+  TDE_DEBUG();
   torch::Tensor ids_to_fetch = torch::from_blob(
                                    ids_to_fetch_.data(),
                                    {num_ids_to_fetch, 2},
                                    torch::dtype(torch::kLong))
-                                   .clone();
+                                   .clone()
+                                   .data();
+  TDE_DEBUG();
   num_ids_to_fetch_.store(0);
   return c10::make_intrusive<TransformResult>(success, ids_to_fetch);
 }
