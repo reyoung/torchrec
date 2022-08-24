54f586b2c98cbe2f0857ab3c35ffb3767d8fb340
Yang Yu <reyoung@126.com>
2022-08-24 11:47:56 +0800

    Add no_grad

--- contrib/dynamic_embedding/src/tde/id_transformer.cpp
+++ contrib/dynamic_embedding/src/tde/id_transformer.cpp
@@ -15,6 +15,7 @@ c10::intrusive_ptr<TransformResult> IDTransformer::Transform(
     c10::intrusive_ptr<TensorList> global_id_list,
     c10::intrusive_ptr<TensorList> cache_id_list,
     int64_t time) {
+  torch::NoGradGuard no_grad;
   TORCH_CHECK(time >= 0);
   TORCH_CHECK(global_id_list->size() == cache_id_list->size());
   transformer_.strategy_.UpdateTime(static_cast<uint32_t>(time));
@@ -26,34 +27,29 @@ c10::intrusive_ptr<TransformResult> IDTransformer::Transform(
     ids_to_fetch_.resize(2 * total_numel);
   } catch (std::bad_alloc& ex) {
     TORCH_CHECK(
-        false,
-        "bad allocate ",
-        ex.what(),
-        " the total_numel=",
-        total_numel);
+        false, "bad allocate ", ex.what(), " the total_numel=", total_numel);
   }
   int64_t num_transformed = 0;
   for (int64_t i = 0; i < global_id_list->size(); ++i) {
     torch::Tensor global_ids = (*global_id_list)[i];
     torch::Tensor cache_ids = (*cache_id_list)[i];
     num_transformed += transformer_.Transform(
-      tcb::span{
-          global_ids.template data_ptr<int64_t>(),
-          static_cast<size_t>(global_ids.numel())},
-      tcb::span{
-          cache_ids.template data_ptr<int64_t>(),
-          static_cast<size_t>(cache_ids.numel())},
-      [this](int64_t global_id, int64_t cache_id) {
-        int64_t idx = num_ids_to_fetch_.fetch_add(1);
-        ids_to_fetch_[2 * idx] = global_id;
-        ids_to_fetch_[2 * idx + 1] = cache_id;
-      });
+        tcb::span{
+            global_ids.template data_ptr<int64_t>(),
+            static_cast<size_t>(global_ids.numel())},
+        tcb::span{
+            cache_ids.template data_ptr<int64_t>(),
+            static_cast<size_t>(cache_ids.numel())},
+        [this](int64_t global_id, int64_t cache_id) {
+          int64_t idx = num_ids_to_fetch_.fetch_add(1);
+          ids_to_fetch_[2 * idx] = global_id;
+          ids_to_fetch_[2 * idx + 1] = cache_id;
+        });
   }
   bool success = num_transformed == total_numel;
   int64_t num_ids_to_fetch = num_ids_to_fetch_.load();
   if (num_ids_to_fetch == 0) {
-    return c10::make_intrusive<TransformResult>(
-        success, torch::Tensor{});
+    return c10::make_intrusive<TransformResult>(success, torch::Tensor{});
   }
   torch::Tensor ids_to_fetch = torch::from_blob(
                                    ids_to_fetch_.data(),
@@ -65,6 +61,7 @@ c10::intrusive_ptr<TransformResult> IDTransformer::Transform(
 }
 
 torch::Tensor IDTransformer::Evict(int64_t num_to_evict) {
+  torch::NoGradGuard no_grad;
   std::vector<int64_t> ids_to_evict = transformer_.Evict(num_to_evict);
   int64_t num_ids_to_evict = ids_to_evict.size() / 2;
   torch::Tensor evicted_ids_tensor =
